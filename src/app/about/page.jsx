884f0556651d7a54efae7dbc6866c62245e64225
rahul <rahuldusa37@gmail.com>
2024-05-14 22:08:37 +0530

    about

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-6'>
+      <h1 className='text-amber-600 text-2xl font-medium'>About</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam laudantium rem fuga commodi, eaque aspernatur dicta dolorem consequuntur beatae sunt delectus necessitatibus et facilis alias officiis? Quibusdam eius dicta possimus.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima harum, nam velit quo beatae, molestias labore culpa excepturi error magnam earum corporis rerum sit eveniet placeat corrupti libero natus. Perferendis?
+      Dolor adipisci eos delectus qui nostrum hic quos itaque dolorem ex autem atque fugiat velit sapiente quidem consectetur et, rem, optio aliquid voluptatum? Vero labore hic maxime explicabo. Error, vitae.</p>
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
         <div className="left flex gap-6 ">
         <MenuItems title="home" address="/" Icon={AiFillHome}/>
-        <MenuItems title="about" address="/" Icon={BsFillInfoCircleFill}/>
+        <MenuItems title="about" address="/about" Icon={BsFillInfoCircleFill}/>
         </div>
         <div className="right flex items-center gap-4">
           <DarkModeSwitch/>
